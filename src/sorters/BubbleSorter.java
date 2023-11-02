a63d3859836a5fc25fa5d3c25ffb6f194e94d8e9
M1h4n1k <mihanik@internet.ru>
2023-11-02 18:39:22 +0200

    changed bubble sort comparisons counter

--- src/sorters/BubbleSorter.java
+++ src/sorters/BubbleSorter.java
@@ -8,13 +8,13 @@ public class BubbleSorter extends Sorter {
 
     public void sort(){
         for (int i = 0; i < arr.length; i++){
-            for (int j = 0; j < arr.length; j++){
+            for (int j = 0; j < i; j++){
                 if (arr[i] < arr[j]){
                     int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
-                    comparisons++;
                 }
+                comparisons++;
             }
         }
     }
